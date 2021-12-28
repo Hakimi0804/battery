4c4e2a45a5ac439dfba81919c0255af8c597ec11
Hakimi0804 <hakimifirdaus944@gmail.com>
2021-12-29 01:13:10 +0800

    utils cleanup

--- battery-utils.sh
+++ battery-utils.sh
@@ -1,13 +1,5 @@
 #!/bin/false 
-# ^~~~~~~~~ to be fixed by termux-fix-shebang
 # shellcheck shell=bash
-# this file will be sourced by the main script (bruh.sh)
-# shellcheck source=bruh.sh
-# make a function that rounds a float (e.g 1.5) to the nearest integer
-# (e.g. 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-round() {
-  echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc));
-}
 
 # colours
 cyan=$(tput setaf 6)
@@ -16,6 +8,9 @@ red=$(tput setaf 1)
 bold_white=$(tput bold; tput setaf 7)
 reset=$(tput sgr 0)
 
+round() {
+  echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc));
+}
 
 calc_wattage() {
   local current=${current//-/};
